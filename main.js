5fccfde7d99f0e5b6888e44b18f080b0b47bd28d
rdelgado134 <rdelgado134@gmail.com>
2024-03-09 20:32:21 -0800

    Rearranged some things for flex box.

--- main.js
+++ main.js
@@ -6,7 +6,48 @@ for(let i = 0; i < 16; i++)
     for(let j = 0; j < 16; j++)
     {
         const box = document.createElement("div");
+        box.id = "test"
         box.classList.add("box");
         containerID.appendChild(box);
     }
+}
+
+/*eventlistener that will change the color of the div when mouse hovers over*/
+function changeColor(event)
+{
+    event.target.style.backgroundColor = "purple";
+}
+
+containerID.addEventListener("mouseover", changeColor);
+
+/*Triggers a prompt asking the user for a number which will be the amount of boxes per side,
+then does the worky work."*/
+function newGrid()
+{
+    const userAnswer = prompt("How many boxes per side?");
+    const requestedNum = parseInt(userAnswer);
+
+    //delete grid before adding a new one
+    deleteGrid();
+
+    
+    for(let i = 0; i < requestedNum; i++)
+    {
+        for(let j = 0; j < requestedNum; j++)
+        {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            containerID.appendChild(box);
+        }
+    }
+}
+
+/*Deletes the grid*/
+function deleteGrid()
+{
+    const testDivs = document.querySelectorAll('#test');
+
+testDivs.forEach(div => {
+    div.parentNode.removeChild(div);
+});
 }
