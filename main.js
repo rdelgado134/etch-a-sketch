643e561eb95f61ba7d9bceccfef218281f0706c7
rdelgado134 <rdelgado134@gmail.com>
2024-03-09 20:33:43 -0800

    Need to add the functions that do the work for our new grid button.

--- main.js
+++ main.js
@@ -1,53 +1,37 @@
-/*nested for loops create 96 div elements with the class "box"*/
-const containerID = document.getElementById("container");
+//make initual 16x16 grid
+const containerRef = document.getElementById("container");
 
 for(let i = 0; i < 16; i++)
 {
     for(let j = 0; j < 16; j++)
     {
-        const box = document.createElement("div");
-        box.id = "test"
-        box.classList.add("box");
-        containerID.appendChild(box);
+        const newBox = document.createElement("div");
+        newBox.id = "individual";
+        newBox.classList.add("test");
+        containerRef.appendChild(newBox);
+
     }
 }
 
-/*eventlistener that will change the color of the div when mouse hovers over*/
-function changeColor(event)
-{
-    event.target.style.backgroundColor = "purple";
-}
-
-containerID.addEventListener("mouseover", changeColor);
-
-/*Triggers a prompt asking the user for a number which will be the amount of boxes per side,
-then does the worky work."*/
-function newGrid()
-{
-    const userAnswer = prompt("How many boxes per side?");
-    const requestedNum = parseInt(userAnswer);
-
-    //delete grid before adding a new one
-    deleteGrid();
-
-    
-    for(let i = 0; i < requestedNum; i++)
-    {
-        for(let j = 0; j < requestedNum; j++)
-        {
-            const box = document.createElement("div");
-            box.classList.add("box");
-            containerID.appendChild(box);
-        }
-    }
-}
-
-/*Deletes the grid*/
+//Delete your current grid
 function deleteGrid()
 {
-    const testDivs = document.querySelectorAll('#test');
+    //Need to check the container div for divs with
+    //the id="individual"; KEEPS THE CONTAINER
+    while(individual)
+    {
+        containerRef.remove(individual);
+    }
+}
 
-testDivs.forEach(div => {
-    div.parentNode.removeChild(div);
-});
+/*Hover effect so that the grid divs change color when your
+mouse passes over them.*/
+
+//const newGridButton = document.getElementById("newGrid");//reference the new grid button
+
+containerRef.addEventListener("mouseover", changeBoxColor);
+
+function changeBoxColor(event)
+{
+    event.target.style.backgroundColor = "red";
 }
